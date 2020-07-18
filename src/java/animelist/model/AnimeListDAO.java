2154b0379581f6e884e13b68d4700feb8c3ede39
BpHana <57785153+BpHana@users.noreply.github.com>
2020-07-18 11:53:23 +0700

    add AnimeList in DAO

--- src/java/animelist/model/AnimeListDAO.java
+++ src/java/animelist/model/AnimeListDAO.java
@@ -1,9 +1,20 @@
 package animelist.model;
 
+import java.util.List;
+
 /**
  *
  * @author Quan Duc Loc CE140037 (SE1401)
  */
 public class AnimeListDAO {
+    List<AnimeDTO> AnimeList;
 
+    public AnimeListDAO() {
+    }
+
+    public List<AnimeDTO> getAnimeList() {
+        return AnimeList;
+    }
+    
+    
 }
