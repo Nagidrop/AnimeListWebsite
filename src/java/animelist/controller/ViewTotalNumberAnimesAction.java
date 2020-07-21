9ac9414b6ab9297fba0ff8a2645249ba4ced93c8
BpHana <57785153+BpHana@users.noreply.github.com>
2020-07-21 14:23:57 +0700

    add button + get Total Animes in List

---
+++ src/java/animelist/controller/ViewTotalNumberAnimesAction.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package animelist.controller;
+
+/**
+ *
+ * @author Tran Minh Thang CE140085
+ */
+public class ViewTotalNumberAnimesAction {
+
+}
